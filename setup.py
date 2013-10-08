f20ee1f29e0f20218a0a7fd1aa1be3b66b216f45
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2013-10-08 20:35:35 +0200

    initial setup.py, still to test

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+
+from distutils.core import setup
+
+setup(
+  name='wposter',
+  version='0.1',
+  description='Python script to post on wordpress blogs',
+  author="Salvo 'LtWorf' Tomaselli",
+  author_email='tiposchi@tiscali.it',
+  scripts=['wposter.py'],
+  license = "GPL3",
+  keywords = "wordpress, editor, terminal",
+  url = "https://github.com/ltworf/wposter",
+  classifiers=[
+  ]
+)
